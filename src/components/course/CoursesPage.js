de6d40ac822d4abbd4a8bf573bcb9a3b173b913a
Eric Freeman <clownhunter@gmail.com>
2016-10-01 18:03:46 -0600

    You can add course now.

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,28 +1,36 @@
 import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
     constructor(props, context) {
         super(props, context);
 
         this.state = {
-            course: {title: ""}
+            course: {title: ''}
         };
 
         this.onTitleChange = (event) => {
             const course = this.state.course;
             course.title = event.target.value;
             this.setState({course: course});
-        }
+        };
 
         this.onClickSave = () => {
-            alert(`Saving ${this.state.course.title}`);
-        }
+            this.props.createCourse(this.state.course);
+            this.setState({course: {title:''}});
+        };
     }    
+
+    courseRow(course, index) {
+        return <div key={index}>{course.title}</div>;
+    }
 
     render() {
         return (
             <div>
                 <h1>Courses</h1>
+                {this.props.courses.map(this.courseRow)}
                 <h2>Add Course</h2>
                 <input type="text" onChange={this.onTitleChange} value={this.state.course.title} />
                 <input type="submit" onClick={this.onClickSave} value="Save" />
@@ -31,4 +39,21 @@ class CoursesPage extends React.Component {
     }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+    courses: PropTypes.array.isRequired,
+    createCourse: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+    return {
+        courses: state.courses
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        createCourse: course => dispatch(courseActions.createCourse(course))
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
